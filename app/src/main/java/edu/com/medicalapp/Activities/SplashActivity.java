7a624c48c3f4966a4ec5e5bddd31900d409219ac
hp <saxenabhu078@gmail.com>
2019-01-06 15:04:44 +0530

    Add DNA Faculty Activity

--- app/src/main/java/edu/com/medicalapp/Activities/SplashActivity.java
+++ app/src/main/java/edu/com/medicalapp/Activities/SplashActivity.java
@@ -30,7 +30,7 @@ public class SplashActivity extends AppCompatActivity {
             public void run() {
                 // This method will be executed once the timer is over
                 // Start your app main activity
-                Intent i = new Intent(SplashActivity.this,FirstloginActivity.class);
+                Intent i = new Intent(SplashActivity.this,DNASuscribeActivity.class);
                 startActivity(i);
 
                 // close this activity
