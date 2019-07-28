9e58c2b64d7d38ec4598636ffddd53f9cf53d037
Lenovo <raj95288@gmail.com>
2019-07-28 18:39:53 +0530

    Hi

---
+++ app/src/main/java/com/dnamedical/Activities/EnterMobileAndEmailActivity.java
@@ -0,0 +1,13 @@
+package com.dnamedical.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class EnterMobileAndEmailActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_mobile_and_email);
+    }
+}
