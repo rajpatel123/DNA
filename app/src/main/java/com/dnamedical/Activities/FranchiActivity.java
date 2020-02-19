4564ccb9131e4f3c1fe9ebc8099b03473f479b76
pks10796 <pks10796@gmail.com>
2020-02-19 10:07:09 +0530

    Added few more fields in franchise

--- app/src/main/java/com/dnamedical/Activities/FranchiActivity.java
+++ app/src/main/java/com/dnamedical/Activities/FranchiActivity.java
@@ -8,6 +8,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import com.dnamedical.Models.franchies.FranchiesResponse;
