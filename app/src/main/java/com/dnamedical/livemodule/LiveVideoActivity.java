47b4a90ca17cbfae662f2ffa14917600f3da9b9d
raj <raj.bahadur@ziploan.biz>
2020-05-02 23:50:28 +0530

    Live Changes

--- app/src/main/java/com/dnamedical/livemodule/LiveVideoActivity.java
+++ app/src/main/java/com/dnamedical/livemodule/LiveVideoActivity.java
@@ -3,6 +3,7 @@ package com.dnamedical.livemodule;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
+import android.view.WindowManager;
 
 import com.dnamedical.R;
 import com.pierfrancescosoffritti.youtubeplayer.player.AbstractYouTubePlayerListener;
@@ -16,7 +17,15 @@ public class LiveVideoActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
         setContentView(R.layout.activity_live_video);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+        getWindow().addFlags(
+                WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED |
+                        WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD |
+                        WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON |
+                        WindowManager.LayoutParams.FLAG_TURN_SCREEN_ON);
+
         liveVideoId = getIntent().getStringExtra("contentId");
 
         initYouTubePlayerView();
