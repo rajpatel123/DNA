0dfd28449358e15c6a475982b6afb95a75979c60
Gujjarharsh <gujjarharshtawar@gmail.com>
2025-01-24 23:19:01 +0530

    gradle successfully

--- app/src/main/java/com/dnamedical/Activities/AboutUsActivit.java
+++ app/src/main/java/com/dnamedical/Activities/AboutUsActivit.java
@@ -1,9 +1,9 @@
 package com.dnamedical.Activities;
 
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
-import android.support.v7.widget.Toolbar;
+//import android.support.v7.widget.Toolbar;
 import android.view.KeyEvent;
 import android.view.MenuItem;
 import android.view.View;
@@ -15,21 +15,25 @@ import android.widget.RelativeLayout;
 import com.dnamedical.R;
 import com.dnamedical.utils.Utils;
 
-import butterknife.BindView;
-import butterknife.ButterKnife;
+//import butterknife.BindView;
+//import butterknife.ButterKnife;
 
 import static android.view.View.GONE;
 
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 public class AboutUsActivit extends AppCompatActivity {
 
-    @BindView(R.id.toolbar)
-    Toolbar toolbar;
+//    @BindView(R.id.toolbar)
+   Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about_us);
-        ButterKnife.bind(this);
+//        ButterKnife.bind(this);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
 
