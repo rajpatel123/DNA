dc2b7312e60cc9c6c11c6eb71f48a3eab93ddab1
Gujjarharsh <gujjarharshtawar@gmail.com>
2025-03-11 11:52:35 +0530

    changes done

---
+++ app/src/main/java/com/dnamedeg/player/EasyVideoProgressCallback.java
@@ -0,0 +1,7 @@
+package com.dnamedeg.player;
+
+/** @author Aidan Follestad (afollestad) */
+public interface EasyVideoProgressCallback {
+
+  void onVideoProgressUpdate(int position, int duration);
+}
